3ec646b405daee4527f6bcfa6c5b1146c71318f2
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-04-29 16:08:04 +0530

    tailwind config added

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import "./global.css";
 
-const index = () => {
+const Homepage = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Homepage</Text>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default Homepage;
